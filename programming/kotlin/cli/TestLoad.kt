85627c83290cdd57f912927e4a4e268a704d44b0
Morten Andersen <dev@accel.dk>
2021-09-06 21:17:28 +0200

    kotlin notes

--- programming/kotlin/cli/TestLoad.kt
+++ programming/kotlin/cli/TestLoad.kt
@@ -1,4 +1,4 @@
 // main function to return the length of array
 // call as `main(arrayOf("elem1", "elem2"))`
-// https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/array-of.html#kotlin$arrayOf(kotlin.Array((kotlin.arrayOf.T)))
+// https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/array-of.html
 fun main(args: Array<String>) = args.size
